24f6b4d61ddeb0fa949b3a4d538924b1a0c0c0ad
appbana <appbana@appbananoMacBook-Pro.local>
2020-07-14 21:12:46 +0900

    :sparkles: checkbox & all_check_button

--- js/content_script.js
+++ js/content_script.js
@@ -1,1 +1,13 @@
-console.log("EGOISTRadioChecker loaded.")
+let a = $("#movie-index > div > div.panel-body > ul > li")
+
+let check_box = $("<input>", {
+    type:"checkbox"
+})
+
+let all_check_btn = $("<button>", {
+    class: "all_check_btn",
+})
+all_check_btn.text("↓")
+
+a.append(check_box)
+a.append(all_check_btn)
