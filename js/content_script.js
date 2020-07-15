c5fc0edf4dbafb5d25ebc14042dab86bc2119279
appbana <appbana@appbananoMacBook-Pro.local>
2020-07-15 21:54:44 +0900

    :sparkles: add check-box id

--- js/content_script.js
+++ js/content_script.js
@@ -1,13 +1,28 @@
-let a = $("#movie-index > div > div.panel-body > ul > li")
+// let plugin_space = $("<div>" {
 
-let check_box = $("<input>", {
-    type:"checkbox"
+// })
+const RADIO_LIST = $("#movie-index > div > div.panel-body > ul > li")
+let abstractNumber = radio_dom => {
+    let a = $(radio_dom).find("a > div > div.col-xs-8.col-sm-10.text > h3")
+    return a[0].textContent.match(/Break the Spell（第(\d+)回配信/)[1]
+}
+
+$.each(RADIO_LIST, (i,v) => {
+    let nth = abstractNumber(v)
+
+    let check_box = $("<input>", {
+        id: `check-box-${nth}`,
+        type: "checkbox"
+    })
+
+    let all_check_btn = $("<button>", {
+        class: "all_check_btn",
+    })
+
+    all_check_btn.text("↓")
+
+    $(v).append(check_box)
+    $(v).append(all_check_btn)
 })
 
-let all_check_btn = $("<button>", {
-    class: "all_check_btn",
-})
-all_check_btn.text("↓")
 
-a.append(check_box)
-a.append(all_check_btn)
