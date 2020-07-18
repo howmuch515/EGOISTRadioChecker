6460817de13ceb3b3349e534e4468b4d7772a5c0
appbana <appbana@appbananoMacBook-Pro.local>
2020-07-18 22:28:02 +0900

    :confetti_ball: complete checkbox.

--- js/content_script.js
+++ js/content_script.js
@@ -1,6 +1,33 @@
-// let plugin_space = $("<div>" {
+let put_check_log = nth => {
+    chrome.storage.sync.get("checked_set", res => {
+        let req_set
+        if (res.checked_set === undefined) {
+            req_set = [nth]
+        } else {
+            req_set = new Set(res.checked_set)
+            req_set.add(nth)
+            req_set = [...req_set]
+        }
+        chrome.storage.sync.set({checked_set: req_set}, () => {
+        });
+    })
+}
 
-// })
+let delete_check_log = nth => {
+    chrome.storage.sync.get("checked_set", res => {
+        let req_set
+        if (res === undefined) {
+            return
+        } else {
+            req_set = new Set(res.checked_set)
+            req_set.delete(nth)
+            req_set = [...req_set]
+        }
+        chrome.storage.sync.set({checked_set: req_set}, () => {
+        });
+    })
+}
+
 const RADIO_LIST = $("#movie-index > div > div.panel-body > ul > li")
 let abstractNumber = radio_dom => {
     let a = $(radio_dom).find("a > div > div.col-xs-8.col-sm-10.text > h3")
@@ -12,17 +39,25 @@ $.each(RADIO_LIST, (i,v) => {
 
     let check_box = $("<input>", {
         id: `check-box-${nth}`,
-        type: "checkbox"
+        class: `watched_checkbox`,
+        type: "checkbox",
+        value: nth
     })
 
-    let all_check_btn = $("<button>", {
-        class: "all_check_btn",
+    chrome.storage.sync.get("checked_set", res => {
+        let a = new Set(res.checked_set)
+        check_box.prop('checked', a.has(nth))
     })
 
-    all_check_btn.text("↓")
+    check_box.change({nth: nth}, event => {
+        let nth = event.data.nth
+        let flag = check_box.prop('checked')
+        if (flag) {
+            put_check_log(nth)
+        } else {
+            delete_check_log(nth)
+        }
+    })
 
-    $(v).append(check_box)
-    $(v).append(all_check_btn)
+    $(v).prepend(check_box)
 })
-
-
