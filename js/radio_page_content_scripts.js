88bbe9fefe7b0e36da9208aa9ef1b4204f89b652
appbana <appbana@appbananoMacBook-Pro.local>
2020-07-30 01:39:57 +0900

    :bug: fix error when access /movie/tag/radio, etc...

--- js/radio_page_content_scripts.js
+++ js/radio_page_content_scripts.js
@@ -9,14 +9,21 @@ let build_text = nth => `EGOISTのラジオ第${nth}回を視聴しました！`
 let hashtag_list = ["EGOIST"]
 
 // main
-let nth = $("meta[property='og:title']").attr("content").match(/Break the Spell（第(\d+)回配信）/)[1]
-let video_url = $("meta[property='og:url']").attr("content").match(/https:\/\/egoist-fc.jp\/movies\/\d+/)
+try {
+    let nth = $("meta[property='og:title']").attr("content").match(/Break the Spell（第(\d+)回配信）/)[1]
+    let video_url = $("meta[property='og:url']").attr("content").match(/https:\/\/egoist-fc.jp\/movies\/\d+/)
 
-let tweet_url = build_tweet_url(build_text(nth), video_url, hashtag_list)
+    let tweet_url = build_tweet_url(build_text(nth), video_url, hashtag_list)
 
-// set tweet button
-let tweet_link = $("<button>").text("Tweet!").click(() => {
-    window.open(tweet_url, "_blank")
-})
+    // set tweet button
+    let tweet_link = $("<button>").text("Tweet!").click(() => {
+        window.open(tweet_url, "_blank")
+    })
 
-$("#movie-show > div > div.panel-heading").append(tweet_link)
+    $("#movie-show > div > div.panel-heading").append(tweet_link)
+}
+catch (e) {
+    // when access "/movie/categories/radio"
+    console.log(`This page is not supported.`)
+    console.log(e)
+}
