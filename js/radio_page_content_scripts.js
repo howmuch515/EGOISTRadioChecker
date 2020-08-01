0562022845501c532f4eb6bedd7cc8c16fe0b443
appbana <appbana@appbananoMacBook-Pro.local>
2020-08-02 01:28:57 +0900

    :gift: utils.js

--- js/radio_page_content_scripts.js
+++ js/radio_page_content_scripts.js
@@ -1,35 +1,5 @@
 const BASE_URL = "https://twitter.com/intent/tweet"
 
-let put_check_log = nth => {
-    chrome.storage.sync.get("checked_set", res => {
-        let req_set
-        if (res.checked_set === undefined) {
-            req_set = [nth]
-        } else {
-            req_set = new Set(res.checked_set)
-            req_set.add(nth)
-            req_set = [...req_set]
-        }
-        chrome.storage.sync.set({checked_set: req_set}, () => {
-        });
-    })
-}
-
-let delete_check_log = nth => {
-    chrome.storage.sync.get("checked_set", res => {
-        let req_set
-        if (res === undefined) {
-            return
-        } else {
-            req_set = new Set(res.checked_set)
-            req_set.delete(nth)
-            req_set = [...req_set]
-        }
-        chrome.storage.sync.set({checked_set: req_set}, () => {
-        });
-    })
-}
-
 let build_tweet_url = (text, url, hashtag_list) => {
     let tweet_url = `${BASE_URL}?text=${encodeURI(text)}&url=${url}&hashtags=${hashtag_list.join("%2C")}`
     return tweet_url
@@ -42,30 +12,7 @@ let hashtag_list = ["EGOIST"]
 try {
     let nth = parseInt($("meta[property='og:title']").attr("content").match(/Break the Spell（第(\d+)回配信）/)[1])
 
-    // checkbox
-    let check_box = $("<input>", {
-        id: `check-box-${nth}`,
-        class: `watched_checkbox`,
-        type: "checkbox",
-        value: nth
-    })
-
-    // check if listened
-    chrome.storage.sync.get("checked_set", res => {
-        let a = new Set(res.checked_set)
-        check_box.prop('checked', a.has(nth))
-    })
-
-    // change event
-    check_box.change({nth: nth}, event => {
-        let nth = event.data.nth
-        let flag = check_box.prop('checked')
-        if (flag) {
-            put_check_log(nth)
-        } else {
-            delete_check_log(nth)
-        }
-    })
+    let check_box = build_checkbox(nth)
 
     // tweet button
     let video_url = $("meta[property='og:url']").attr("content").match(/https:\/\/egoist-fc.jp\/movies\/\d+/)
