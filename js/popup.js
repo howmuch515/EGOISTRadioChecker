1bd6f9553c247c96674546160d0b6f401db5e9e5
appbana <appbana@appbanaMBPprivate.local>
2020-08-26 01:34:18 +0900

    :confetti_ball: add radio_title to tweet.

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 $("#clear-button").click(function() {
     if (confirm("EGOISTCRadioCheckerの記録を全て削除しますか？")) {
-        for (let i in STORAGE_NAMES) {
-            let delete_key = STORAGE_NAMES[i]
+        for (let i of STORAGE_NAMES) {
+            let delete_key = i.id
             clear_check_log(delete_key)
         }
     }
